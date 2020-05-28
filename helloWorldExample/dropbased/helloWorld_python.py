85dd5dfa7fe3e8352691f56615d388f6b49b8901
pritchardn <21726929@student.uwa.edu.au>
2020-05-28 16:07:20 +0800

    Adds rudimentary python example
    There is extra data being written to file. This might be to do with how the function is serialized.

---
+++ helloWorldExample/dropbased/helloWorld_python.py
@@ -0,0 +1,37 @@
+import os
+import time
+
+from dlg.apps.pyfunc import PyFuncApp
+from dlg.ddap_protocol import DROPStates
+from dlg.drop import FileDROP
+from dlg.droputils import allDropContents
+
+output_fname = os.getcwd() + '/result6.out'
+
+
+#  TODO: Investigate extra data written to file
+def hello_world():
+    return "Hello World"
+
+
+fname = 'helloWorldExample.dropbased.helloWorld_python.hello_world'
+
+# Initialize our Drops
+a = PyFuncApp('a', 'a', func_name=fname)
+b = FileDROP('b', 'b', filepath=output_fname)
+
+# Link Drops together
+a.addOutput(b)
+
+# Execute and wait (HACK)
+a.async_execute()
+time.sleep(5)
+
+# Inspect Results
+for drop in (a, b):
+    print(drop)
+    print(drop.status)
+    print(drop.status == DROPStates.COMPLETED)
+
+# Check the file was written correctly
+print(allDropContents(b))
