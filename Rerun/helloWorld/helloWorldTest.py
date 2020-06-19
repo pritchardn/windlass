15e4b3e81687ff79544ca0b00868b9da72d4cfc1
pritchardn <nicholas.pritchard@research.uwa.edu.au>
2020-06-19 11:37:51 +0800

    New completedrop method is implemented. Now any listener to 'dropCompleted' should receive reproducibility data.

--- Rerun/helloWorld/helloWorldTest.py
+++ Rerun/helloWorld/helloWorldTest.py
@@ -4,7 +4,7 @@
 import optparse
 import pathlib
 
-from dlg.translator.tool_commands import dlg_fill, dlg_unroll, dlg_partition, dlg_map
+from dlg.translator.tool_commands import dlg_fill, dlg_unroll, dlg_partition, dlg_map, dlg_submit
 
 HOME = pathlib.Path(__file__).parent.absolute()
 TEMP = HOME / "temp/"
@@ -29,4 +29,4 @@ dlg_partition(parser, ['-P', pgt, '-o', pgs, '-f', 'newline'])
 parser = optparse.OptionParser()
 dlg_map(parser, ['-P', pgs, '-N', '127.0.0.1,127.0.0.1', '-o', pg, '-f', 'newline'])
 parser = optparse.OptionParser()
-# dlg_submit(parser, ['-P', pg, '-p', '8000', '-w'])
+dlg_submit(parser, ['-P', pg, '-p', '8000', '-w'])
