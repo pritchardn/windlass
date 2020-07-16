bbb0c9c124f170429c544f302ea3a761bdfb2c7d
pritchardn <21726929@student.uwa.edu.au>
2020-07-16 16:52:38 +0800

    Driver code now checks if final leaf values are identical.

--- Experiments/Rerun/HelloWorld1/rerunDriver.py
+++ Experiments/Rerun/HelloWorld1/rerunDriver.py
@@ -3,6 +3,7 @@
 # Ensure a DALiuGE Master Manager is running on port 9000
 # Ensure a DALiuGE Daemon is running on port 8000
 
+import json
 import optparse
 
 from dlg.common.reproducibility.reproducibility import ReproduciblityFlags
@@ -31,12 +32,19 @@ def run_full_workflow(workflow: str, rmode: ReproduciblityFlags):
     dlg_submit(parser, ['-P', pg, '-p', '8000', '-R', '-o', rg, '-f', 'newline'])
 
 
-def summarise_run(workflow: str):
-    return {}
+def compare_runs(w1: str, w2: str):
+    f1 = open(w1 + '.out')
+    f2 = open(w2 + '.out')
+    r1 = json.load(f1)
+    r2 = json.load(f2)
+    f1.close()
+    f2.close()
+
+    return set(r1['reprodata']['leaves']) == set(r2['reprodata']['leaves'])
 
 
 if __name__ == "__main__":
-    #run_full_workflow("HelloSBash", ReproduciblityFlags.RERUN)
+    run_full_workflow("HelloSBash", ReproduciblityFlags.RERUN)
     run_full_workflow("HelloWorldBash", ReproduciblityFlags.RERUN)
-    #summarise_run("HelloSBash")
-    #summarise_run("HelloWorldBash")
+    result = compare_runs("HelloSBash", "HelloWorldBash")
+    print(result)
