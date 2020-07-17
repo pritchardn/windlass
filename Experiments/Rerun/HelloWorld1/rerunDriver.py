c0e10c940493c0ead0b703cff9ba5f86e7ca25a3
pritchardn <21726929@student.uwa.edu.au>
2020-07-17 14:43:26 +0800

    Splits out testing code for Re-Use

--- Experiments/Rerun/HelloWorld1/rerunDriver.py
+++ Experiments/Rerun/HelloWorld1/rerunDriver.py
@@ -3,48 +3,12 @@
 # Ensure a DALiuGE Master Manager is running on port 9000
 # Ensure a DALiuGE Daemon is running on port 8000
 
-import json
-import optparse
-
 from dlg.common.reproducibility.reproducibility import ReproduciblityFlags
-from dlg.translator.tool_commands import dlg_fill, dlg_unroll, dlg_partition, dlg_map, dlg_submit
 
-
-def run_full_workflow(workflow: str, rmode: ReproduciblityFlags):
-    lgt = workflow + ".graph"
-    lg = workflow + "LG.graph"
-    pgs = workflow + "PGS.graph"
-    pgt = workflow + "PGT.graph"
-    pg = workflow + "PG.graph"
-    rg = workflow + ".out"
-
-    rmodes = str(rmode.value)
-
-    parser = optparse.OptionParser()
-    dlg_fill(parser, ['-L', lgt, '-R', rmodes, '-o', lg, '-f', 'newline'])
-    parser = optparse.OptionParser()
-    dlg_unroll(parser, ['-L', lg, '-o', pgs, '-f', 'newline'])
-    parser = optparse.OptionParser()
-    dlg_partition(parser, ['-P', pgs, '-o', pgt, '-f', 'newline'])
-    parser = optparse.OptionParser()
-    dlg_map(parser, ['-P', pgt, '-N', '127.0.0.1,127.0.0.1', '-o', pg, '-f', 'newline'])
-    parser = optparse.OptionParser()
-    dlg_submit(parser, ['-P', pg, '-p', '8000', '-R', '-o', rg, '-f', 'newline'])
-
-
-def compare_runs(w1: str, w2: str):
-    f1 = open(w1 + '.out')
-    f2 = open(w2 + '.out')
-    r1 = json.load(f1)
-    r2 = json.load(f2)
-    f1.close()
-    f2.close()
-
-    return set(r1['reprodata']['leaves']) == set(r2['reprodata']['leaves'])
-
+from Experiments.labTools import run_full_workflow, test_identical
 
 if __name__ == "__main__":
     run_full_workflow("HelloSBash", ReproduciblityFlags.RERUN)
     run_full_workflow("HelloWorldBash", ReproduciblityFlags.RERUN)
-    result = compare_runs("HelloSBash", "HelloWorldBash")
+    result = test_identical("HelloSBash", "HelloWorldBash")
     print(result)
---
+++ Experiments/labTools.py
@@ -0,0 +1,38 @@
+import json
+import optparse
+
+from dlg.common.reproducibility.reproducibility import ReproduciblityFlags
+from dlg.translator.tool_commands import dlg_fill, dlg_unroll, dlg_partition, dlg_map, dlg_submit
+
+
+def run_full_workflow(workflow: str, rmode: ReproduciblityFlags):
+    lgt = workflow + ".graph"
+    lg = workflow + "LG.graph"
+    pgs = workflow + "PGS.graph"
+    pgt = workflow + "PGT.graph"
+    pg = workflow + "PG.graph"
+    rg = workflow + ".out"
+
+    rmodes = str(rmode.value)
+
+    parser = optparse.OptionParser()
+    dlg_fill(parser, ['-L', lgt, '-R', rmodes, '-o', lg, '-f', 'newline'])
+    parser = optparse.OptionParser()
+    dlg_unroll(parser, ['-L', lg, '-o', pgs, '-f', 'newline'])
+    parser = optparse.OptionParser()
+    dlg_partition(parser, ['-P', pgs, '-o', pgt, '-f', 'newline'])
+    parser = optparse.OptionParser()
+    dlg_map(parser, ['-P', pgt, '-N', '127.0.0.1,127.0.0.1', '-o', pg, '-f', 'newline'])
+    parser = optparse.OptionParser()
+    dlg_submit(parser, ['-P', pg, '-p', '8000', '-R', '-o', rg, '-f', 'newline'])
+
+
+def test_identical(w1: str, w2: str):
+    f1 = open(w1 + '.out')
+    f2 = open(w2 + '.out')
+    r1 = json.load(f1)
+    r2 = json.load(f2)
+    f1.close()
+    f2.close()
+
+    return set(r1['reprodata']['leaves']) == set(r2['reprodata']['leaves'])
