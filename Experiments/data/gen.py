e2a6bd1ec982eda28264733ba8c46fba5eeeb0d8
pritchardn <21726929@student.uwa.edu.au>
2020-10-07 13:24:15 +0800

    Abstracts simple number file generation

--- Experiments/data/gen.py
+++ Experiments/data/gen.py
@@ -1,7 +1,13 @@
 import pickle
-a = [1, 2, 3, 4]
-with open('numbers1.in', 'wb') as handle:
-    pickle.dump(a, handle)
-with open('numbers1.in', 'rb') as handle:
-    b = pickle.load(handle)
-print(a == b)
+
+
+def write_nums(nums, filename):
+    with open(filename + '.in', 'wb') as handle:
+        pickle.dump(nums, handle)
+    with open(filename + '.in', 'rb') as handle:
+        b = pickle.load(handle)
+    print(nums == b)
+
+
+write_nums([1, 2, 3, 4], 'numbers1')
+write_nums([4, 3, 2, 1], 'numbers2')
