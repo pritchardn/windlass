0070847bf1a845061ec48419ced6d4cc26e090bd
pritchardn <21726929@student.uwa.edu.au>
2021-01-20 16:52:30 +0800

    Adds several lowpass filter trials

---
+++ Experiments/EX7LowPass1/EX7Driver.py
@@ -0,0 +1,9 @@
+# (c) Nicholas Pritchard 2020
+# A preliminary Python script to actually test reproducibility between two DALiuGE Workflows
+# Ensure a DALiuGE Master Manager is running on port 9000
+# Ensure a DALiuGE Daemon is running on port 8000
+
+from Experiments.tools.labTools import full_trial
+
+if __name__ == "__main__":
+    full_trial('numpy_fft', 'cuda_fft', '../../lowpass_graphs/')
