e4c08d7703569edd67582497e69e26921410a720
pritchardn <21726929@student.uwa.edu.au>
2021-08-26 14:34:26 +0800

    EX16Driver.py now runs the graph 10 times.

--- Experiments/EX16Threading/EX16Driver.py
+++ Experiments/EX16Threading/EX16Driver.py
@@ -2,17 +2,13 @@
 # Ensure a DALiuGE Master Manager is running on port 9000
 # Ensure a DALiuGE Daemon is running on port 8000
 
-# Note, input data comes from https://developer.skatelescope.org/projects/icrar-leap-accelerate/en/latest/md/Docker.html
-# This is also useful https://developer.skao.int/_/downloads/icrar-leap-accelerate/en/stable/pdf/
-
 from Experiments.tools.labTools import single_trial_single
 
 
-def main(methods):
-    for method in methods:
+def main(method):
+    for _ in range(10):
         single_trial_single(method, './graphs/', './' + method + '_')
 
 
 if __name__ == "__main__":
-    graphs = ['thrashThread4']
-    main(graphs)
+    main('thrashThread4')
